91b9448fc571863fe43476cd628093a221d1ecb0
nicoluna319 <nicolaslunaromero@gmail.com>
2024-04-18 09:34:54 -0500

    18/04

--- src/main/java/com/example/primeraweb/controller/CoderController.java
+++ src/main/java/com/example/primeraweb/controller/CoderController.java
@@ -4,13 +4,19 @@ package com.example.primeraweb.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.primeraweb.entity.Coder;
 import com.example.primeraweb.services.CoderService;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 
 
@@ -25,9 +31,10 @@ public class CoderController {
     /*Metodo para mostrar la lista y enviarle la lista de coders */
 
     @GetMapping
-    public String showViewGetAll(Model objModel){
+    public String showViewGetAll(Model objModel, @RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "3")int size){
 
-        List<Coder> list = this.objCoderService.findAll();
+        Page<Coder> list = this.objCoderService.fingPaginated(page -1, size);
+        objModel.addAttribute("coderList",list);
         objModel.addAttribute("coderList",list);
 
 
@@ -35,8 +42,51 @@ public class CoderController {
     }
 
     @GetMapping("/form")
-    public String showViewFormCoder(){
+    public String showViewFormCoder(Model objModel){
+        objModel.addAttribute("coderList", list);
+        objModel.addAttribute("currentPage", "page");
+        objModel.addAttribute("totalPages", "page");
         return "viewForm";
     }
+
+    /*Metodo para mostrar el formulario de actualizar un coder */
+
+    @GetMapping("/update/{id}")
+    public String showFormUpdate(@PathVariable Long id ,Model objModel){
+        
+        Coder objCoderFind = this.objCoderService.findById(id);
+        objModel.addAttribute("coder", objCoderFind);
+        objModel.addAttribute("action", "/edit/" + id);
+        return "viewForm";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String updateCoder(@PathVariable Long id, @ModelAttribute Coder objCoder){
+        this.objCoderService.update(id,objCoder);
+        return "redirect:/";
+
+    }
+
+    
+    //Metodo para insertar un coder mediante el verbo post
+    //@ModelAttribute se encarga de obtener la informacion enviada desde la vista
+    @PostMapping("/coder/create")
+    public String createCoder(@ModelAttribute Coder objCoder) {
+
+        this.objCoderService.insert(objCoder);
+        return "redirect:/";
+    }
+
+    /*Metodo para borrar */
+    @GetMapping("/delete/{id}")
+    public String deleteCoder(@PathVariable Long id, @ModelAttribute Coder objCoder){
+        this.objCoderService.delete(id,objCoder);
+        return "redirect:/";
+
+    }
+    
+
+
+    
 
 }
--- src/main/java/com/example/primeraweb/services/CoderService.java
+++ src/main/java/com/example/primeraweb/services/CoderService.java
@@ -3,6 +3,9 @@ package com.example.primeraweb.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.example.primeraweb.entity.Coder;
@@ -20,6 +23,45 @@ public class CoderService {
     public List<Coder> findAll(){
         return this.objCoderRepository.findAll();
     }
+
+    public Page<Coder> fingPaginated(int page, int size){
+        if(page< 0){
+            page = 1;
+        }
+
+        /*Crear objeto de paginacion */
+
+        Pageable objPageable= PageRequest.of(page, size);
+        return this.objCoderRepository.findAll(objPageable);
+    }
+/*Servicio para guardar un coder */
+    public Coder insert (Coder objCoder){
+        return this.objCoderRepository.save(objCoder);
+    }
+/*Servicio para actualizar un coder */
+
+public Coder update(Long id, Coder objCoder){
+    /*Buscar al coder con ese id */
+
+
+    Coder objCoderDB = this.findById(id);
+    if(objCoder == null) return null;
+    objCoderDB = objCoder;
+    return this.objCoderRepository.save(objCoderDB);
+}
+
+public Coder findById(Long id){
+    return this.objCoderRepository.findById(id).orElse(null);
+}
+
+public void delete(Long id, Coder objCoder){
+
+
+    Coder objCoderDB = this.findById(id);
+
+    this.objCoderRepository.delete(objCoderDB);
+}
+
 
 
 }
